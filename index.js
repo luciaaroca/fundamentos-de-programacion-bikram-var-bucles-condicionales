07215f2d60b4a63f93a2513ddb895c7c948d3041
Lucía Aroca Solís <luciaarocasolis@Lucias-MacBook-Air.local>
2025-09-29 16:26:02 +0200

    var-bucles-condic-repositorio

---
+++ index.js
@@ -0,0 +1,71 @@
+//VARIABLES
+let variableSinValor;
+let booleano1= true;
+let booleano2= false;
+const PI= 3.14;
+const TAU= 2 * PI;
+
+//BOOLEANOS
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+
+//OPERADORES
+let incrementarDesp = 2;
+let resultadoDesp= incrementarDesp ++;
+
+let incrementarAntes = 2;
+let resultadoAntes= ++ incrementarAntes;
+
+//BUCLES
+let constarHasta10_2 = 0;
+for(let i= 0; i<10; i++){constarHasta10_2++};
+
+let postI=0;
+let postJ=0;
+for(let i=0; i<11; i++){postI=postI + postJ++};
+
+let sumaPares = 0;
+for (let i=0; i<10; i++){
+    if(i % 2 === 0){sumaPares+=i;}
+    else {continue;}
+    }
+//PROYECTOINDIVIDUAL
+//VARIABLES
+
+let variableValorNumerico= 10;
+const MiNombre= "Lucía";
+const MiNumeroFav = 2;
+
+//BOOLEANO
+let booleanoOr= booleano1 || booleano2;
+let booleanoMix1= 
+(booelano1 && TAU/2 === PI) || (variableValorNumerico>= miNumeroFav);
+
+let seisNoEsNueve = 6 !== 9;
+
+let booleanoMix2 = variableValorNumerico>0 || variableValorNumerico <- (miNumeroFav*TAU);
+
+//OPERADORES
+let valorSuma = miNumeroFav + variableValorNumerico;
+let valorResta = miNumeroFav - variableValorNumerico;
+let valorMultiplicacion = miNumeroFav * variableValorNumerico;
+let valorDivision = miNumeroFav/ 3;
+
+//BUCLES
+let contarHasta10 = 0
+while (contarHasta10<10){
+    contarHasta10++;
+}
+let preI= 0;
+let perJ = 0;
+
+for (let i = 0; i<11; i++){
+    preI += ++perJ;
+}
+
+let sumaImpares = 0;
+for (let i= 0; i<10; i++){
+    if (i % 2 !== 0){sumImpares += +i;}
+    else {continue;}
+}
