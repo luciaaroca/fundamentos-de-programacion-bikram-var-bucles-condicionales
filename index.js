597143d3b0b68b4003be264c1204c1044405856c
Lucía Aroca Solís <luciaarocasolis@Lucias-MacBook-Air.local>
2025-09-30 09:09:47 +0200

    numeraciones ejercicios

--- index.js
+++ index.js
@@ -1,69 +1,95 @@
 //VARIABLES
+//1
 let variableSinValor;
+//2
 let booleano1= true;
 let booleano2= false;
+//3
 const PI= 3.14;
+//4
 const TAU= 2 * PI;
 
 //BOOLEANOS
+//5
 let booleanoAnd = booleano1 && booleano2;
+//6
 let booleanoNot = !booleano1;
+//7
 let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
 
 //OPERADORES
-let incrementarDesp = 2;
+//8
+let incrementarDesp
+incrementarDesp = 2;
 let resultadoDesp= incrementarDesp ++;
-
-let incrementarAntes = 2;
+//9
+let incrementarAntes
+incrementarAntes = 2;
 let resultadoAntes= ++ incrementarAntes;
 
 //BUCLES
-let constarHasta10_2 = 0;
-for(let i= 0; i<10; i++){constarHasta10_2++};
-
+//10
+let contarHasta10_2 = 0;
+for(let i= 0; i<10; i++){
+    contarHasta10_2++
+}
+//11
 let postI=0;
 let postJ=0;
 for(let i=0; i<11; i++){postI=postI + postJ++};
 
+//12
 let sumaPares = 0;
 for (let i=0; i<10; i++){
-    if(i % 2 === 0){sumaPares+=i;}
-    else {continue;}
+    if(i % 2 == 0){
+        sumaPares+=i;
+    } else {continue;   
+       }
     }
 //PROYECTOINDIVIDUAL
 //VARIABLES
-
+//13
 let variableValorNumerico= 10;
+//14
 const MiNombre= "Lucía";
+//15
 const MiNumeroFav = 2;
 
 //BOOLEANO
+//16
 let booleanoOr= booleano1 || booleano2;
+//17
 let booleanoMix1= 
-(booelano1 && TAU/2 === PI) || (variableValorNumerico>= miNumeroFav);
-
+(booelano1 && TAU/2 === PI) || variableValorNumerico>= MiNumeroFav;
+//18
 let seisNoEsNueve = 6 !== 9;
-
-let booleanoMix2 = variableValorNumerico>0 || variableValorNumerico <- (miNumeroFav*TAU);
+//19
+let booleanoMix2 = variableValorNumerico>0 || variableValorNumerico <- (MiNumeroFav*TAU);
 
 //OPERADORES
-let valorSuma = miNumeroFav + variableValorNumerico;
-let valorResta = miNumeroFav - variableValorNumerico;
-let valorMultiplicacion = miNumeroFav * variableValorNumerico;
-let valorDivision = miNumeroFav/ 3;
+//20
+let valorSuma = MiNumeroFav + variableValorNumerico;
+//21
+let valorResta = MiNumeroFav - variableValorNumerico;
+//22
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+//23
+let valorDivision = MiNumeroFav/ 3;
 
 //BUCLES
-let contarHasta10 = 0
+//24
+let contarHasta10 = 0;
 while (contarHasta10<10){
     contarHasta10++;
 }
-let preI= 0;
-let perJ = 0;
+//25
+let preI=0;
+let perJ=0;
 
 for (let i = 0; i<11; i++){
     preI += ++perJ;
 }
-
+//26
 let sumaImpares = 0;
 for (let i= 0; i<10; i++){
     if (i % 2 !== 0){sumImpares += +i;}
